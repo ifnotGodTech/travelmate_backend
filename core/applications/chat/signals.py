b6ea4fa539f93c6e7dbbc37695e8859669ef74c5
ClemsNzube <clemsnzube@gmail.com>
2025-05-07 04:48:22 +0100

    TICKET UPDATE

--- core/applications/chat/signals.py
+++ core/applications/chat/signals.py
@@ -2,13 +2,31 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
+from django.conf import settings
 from .models import ChatMessage, ChatSession
+from core.applications.users.models import Profile
 
 @receiver(post_save, sender=ChatMessage)
 def notify_new_message(sender, instance, created, **kwargs):
     """Send notification when a new message is created"""
     if created:
         channel_layer = get_channel_layer()
+
+        # Fetch profile data
+        try:
+            profile = Profile.objects.get(user=instance.sender)
+            profile_data = {
+                'first_name': profile.first_name or '',
+                'last_name': profile.last_name or '',
+                'profile_pics': profile.get_profile_picture
+            }
+        except Profile.DoesNotExist:
+            profile_data = {
+                'first_name': '',
+                'last_name': '',
+                'profile_pics': f'{settings.STATIC_URL}images/avatar.png'
+            }
+
         # Notify the chat room
         async_to_sync(channel_layer.group_send)(
             f'chat_{instance.session.id}',
@@ -17,7 +35,9 @@ def notify_new_message(sender, instance, created, **kwargs):
                 'id': instance.id,
                 'message': instance.content,
                 'sender_id': instance.sender.id,
-                'sender_username': instance.sender.username,
+                'first_name': profile_data['first_name'],
+                'last_name': profile_data['last_name'],
+                'profile_pics': profile_data['profile_pics'],
                 'is_staff': instance.sender.is_staff,
                 'created_at': instance.created_at.isoformat(),
             }
@@ -33,10 +53,21 @@ def notify_session_update(sender, instance, **kwargs):
     }
 
     if instance.assigned_admin:
-        session_data['assigned_admin'] = {
-            'id': instance.assigned_admin.id,
-            'username': instance.assigned_admin.username
-        }
+        try:
+            profile = Profile.objects.get(user=instance.assigned_admin)
+            session_data['assigned_admin'] = {
+                'id': instance.assigned_admin.id,
+                'first_name': profile.first_name or '',
+                'last_name': profile.last_name or '',
+                'profile_pics': profile.get_profile_picture
+            }
+        except Profile.DoesNotExist:
+            session_data['assigned_admin'] = {
+                'id': instance.assigned_admin.id,
+                'first_name': '',
+                'last_name': '',
+                'profile_pics': f'{settings.STATIC_URL}images/avatar.png'
+            }
 
     # Notify the chat room
     async_to_sync(channel_layer.group_send)(
