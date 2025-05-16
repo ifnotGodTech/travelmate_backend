0dcb2d3d991be64d19c8c0692fc788722f2f5286
Emmanuel Ademola <mailemmydee@gmail.com>
2025-05-16 20:05:37 +0000

    Refactor user API router to include additional views and improve organization

--- core/applications/users/api/routers.py
+++ core/applications/users/api/routers.py
@@ -1,11 +1,13 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter
 from rest_framework.routers import SimpleRouter
+from django.urls import path
 
 from core.applications.users.api.views import (
     SuperUserViewSet, UserViewSet,  ProfileViewSet,
-    UserRegistrationViewSet, AdminRegistrationViewSet
-
+    UserRegistrationViewSet, AdminRegistrationViewSet,
+    SuperAdminViewSet, RoleViewSet, GroupedPermissionsView,
+    AcceptInvitationView, ValidateInvitationTokenView
 )
 PREFIX = "users"
 
@@ -30,5 +32,14 @@ router.register(
 router.register("profile", ProfileViewSet, basename="profile")
 router.register("superuser", SuperUserViewSet, basename="admin-users")
 
+router.register("roles", RoleViewSet, basename="roles")
+router.register("superadmins", SuperAdminViewSet, basename="superadmins")
+
 app_name = f"{PREFIX}"
 urlpatterns = router.urls
+urlpatterns += [
+    path("permissions/grouped/", GroupedPermissionsView.as_view(), name="grouped-permissions"),
+    path("invitation/accept/", AcceptInvitationView.as_view(), name="invitation-accept"),
+    path("invitation/validate/", ValidateInvitationTokenView.as_view(), name="invitation-validate"),
+
+]
