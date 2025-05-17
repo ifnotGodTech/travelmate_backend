415c27e7ba3c42356a18b658df6e079831d3a533
ClemsNzube <clemsnzube@gmail.com>
2025-05-17 16:16:56 +0100

    FIXES

--- core/applications/users/urls.py
+++ core/applications/users/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from .views import user_detail_view
 from .views import user_redirect_view
 from .views import user_update_view, superuser_signup
-from . import views
 
 app_name = "users"
 urlpatterns = [
@@ -11,5 +10,4 @@ urlpatterns = [
     path("~update/", view=user_update_view, name="update"),
     path("<int:pk>/", view=user_detail_view, name="detail"),
     path("superuser/",view=superuser_signup, name="superuser_signup"),
-    path('reactivate-superuser/', views.reactivate_superuser, name='reactivate_superuser'),
 ]
